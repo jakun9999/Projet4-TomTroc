bc91455e1be2674cd9edc2203cb1662f4acb6e3e
Jakun9999 <matthieulucas457@outlook.fr>
2026-05-14 08:19:58 +0200

    :hammer: #1 - init.sql file prepared

---
+++ docker/mysql/init.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : tomtroc_db:3306
+-- Généré le : jeu. 14 mai 2026 à 05:54
+-- Version du serveur : 8.0.36
+-- Version de PHP : 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tomtroc`
+--
+CREATE DATABASE IF NOT EXISTS `tomtroc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `tomtroc`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `book`
+--
+
+CREATE TABLE `book` (
+  `id` int NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `author_firstname` varchar(128) NOT NULL,
+  `author_lastname` varchar(128) NOT NULL,
+  `author_pseudo` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `status` varchar(32) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `discussion`
+--
+
+CREATE TABLE `discussion` (
+  `id` int NOT NULL,
+  `user_1_id` int NOT NULL,
+  `user_2_id` int NOT NULL,
+  `creation_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `discussion_id` int NOT NULL,
+  `content` text NOT NULL,
+  `is_read` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `pseudo` varchar(128) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `creation_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `discussion`
+--
+ALTER TABLE `discussion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_1_id` (`user_1_id`),
+  ADD KEY `user_2_id` (`user_2_id`);
+
+--
+-- Index pour la table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `discussion_id` (`discussion_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `discussion`
+--
+ALTER TABLE `discussion`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `discussion`
+--
+ALTER TABLE `discussion`
+  ADD CONSTRAINT `discussion_ibfk_1` 
+  FOREIGN KEY (`user_1_id`) 
+  REFERENCES `user` (`id`)
+  ON DELETE CASCADE,
+  ADD CONSTRAINT `discussion_ibfk_2` 
+  FOREIGN KEY (`user_2_id`) 
+  REFERENCES `user` (`id`)
+  ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` 
+  FOREIGN KEY (`user_id`) 
+  REFERENCES `user` (`id`)
+  ON DELETE CASCADE,
+  ADD CONSTRAINT `message_ibfk_2` 
+  FOREIGN KEY (`discussion_id`) 
+  REFERENCES `discussion` (`id`)
+  ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_ibfk_1` 
+  FOREIGN KEY (`user_id`) 
+  REFERENCES `user` (`id`)
+  ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
