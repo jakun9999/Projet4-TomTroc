b625bdfc280e521fcf424f5f59a95c41b4a2455e
Jakun9999 <matthieulucas457@outlook.fr>
2026-06-03 09:34:27 +0200

    :construction: BooksController - Add methods for displaying book list, editing and adding books.

--- www/src/Controllers/BooksController.php
+++ www/src/Controllers/BooksController.php
@@ -12,21 +12,35 @@ use Ml\App\Views\View;
  */
 class BooksController
 {
+    /**
+     * Display the list of books available for exchange.
+     */
     public function showBooks(): void
     {
         $view = new View('TomTroc - Nos livres');
         $view->render('books');
     }
 
+    /**
+     * Display the form to edit an existing book.
+     */
     public function editBook(): void
     {
         $view = new View('TomTroc - Edition du livre');
         $view->render('edit-book', ['mode' => 'edit']);
     }
 
-    public function addBook(): void
+    /**
+     * Display the form to add a new book.
+     */
+    public function newBook(): void
     {
         $view = new View('TomTroc - Ajouter un livre');
-        $view->render('edit-book', ['mode' => 'add']);
+        $view->render('edit-book', ['mode' => 'new']);
     }
+
+    /**
+     * Add a new book to the collection.
+     */
+    public function addBook(): void {}
 }
