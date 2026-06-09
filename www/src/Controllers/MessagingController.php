c54986c3e2e786ed3b6938ab53d41c334531c343
Jakun9999 <matthieulucas457@outlook.fr>
2026-06-09 13:44:08 +0200

    :construction: Messaging - template implementation.

--- www/src/Controllers/MessagingController.php
+++ www/src/Controllers/MessagingController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ml\App\Controllers;
 
+use Ml\App\Models\DiscussionManager;
 use Ml\App\Views\View;
 
 /**
@@ -12,9 +13,36 @@ use Ml\App\Views\View;
  */
 class MessagingController
 {
+
+    /**
+     * Simply render messaging page with latest discussion message selected.
+     * 
+     */
     public function showMessaging(): void
     {
+        // If visitor is not an authenticated user, we redirect him
+        // to login page.
+        if (!isset($_SESSION['user'])) {
+            header('location: /login');
+            exit();
+        }
+
+        $discussionManager = new DiscussionManager();
+        $discussions = $discussionManager->getAllDiscussionByUserId($_SESSION['user']->getId());
         $view = new View('TomTroc - Messagerie');
-        $view->render('messaging');
+        $view->render('messaging', ['discussions' => $discussions]);
     }
+
+    /**
+     * Display the messaging page with a new open discussion with a specific
+     * user (used for example when coming from book detail page via send 
+     * message button).
+     */
+    public function prepareMessage(): void {}
+
+    /**
+     * Called when the message form is used to manage recording and display of 
+     * a new message sent by the user.
+     */
+    public function sendMessage(): void {}
 }
--- www/templates/messaging.php
+++ www/templates/messaging.php
@@ -6,9 +6,16 @@
  */
 
 /** @var array $params */
-if (isset($_SESSION['user'])) {
-    $discussions = [];
+if (!isset($_SESSION['user'])) {
+    header('location: /login');
 }
+
+if (isset($params['discussions'])) {
+    $discussions = $params['discussions'];
+}
+
+$userPseudo = $_SESSION['user']->getPseudo();
+$userId = $_SESSION['user']->getId();
 
 ?>
 <section class="grow flex flex-col items-center w-full h-auto bg-cassian-secondary xl:bg-cassian-primary max-w-94.25 xl:max-w-cassian-1440 mx-auto">
@@ -16,7 +23,47 @@ if (isset($_SESSION['user'])) {
         <!-- Discussion list -->
         <div class="flex flex-col w-83.75 xl:w-77 min-h-full bg-cassian-secondary">
             <h1 class="xl:ml-8.5 mt-13.75 font-cassian-playfair text-[26px]">Messagerie</h1>
-            <div class="flex flex-col"></div>
+            <div class="flex flex-col">
+                <?php foreach ($discussions as $discussion): ?>
+                    <?php
+                    // For each discussion we load the user with whom
+                    // current use has exchanged, his/her photo url,
+                    // last message content and date.
+                    if ($discussion->getUser1Pseudo() === $userPseudo) {
+                        $otherUserPseudo = $discussion->getUser2Pseudo();
+                        $otherUserPhoto = $discussion->getUser2Photo();
+                    } else {
+                        $otherUserPseudo = $discussion->getUser1Pseudo();
+                        $otherUserPhoto = $discussion->getUser1Photo();
+                    }
+
+                    $lastMessageKey = array_key_last($discussion->getMessages());
+                    $message = $discussion->getMessages()[$lastMessageKey];
+                    $lastMessageContent = $message->getContent();
+                    $lastMessageDate = Ml\App\Services\Utils::displayDiscussionDate($message->getCreationDate());
+
+                    ?>
+                    <div class="flex pl-8.5 pt-4.5 pr-10.5 pb-4.5">
+                        <img src="<?= $otherUserPhoto !== '' ?
+                                        htmlspecialchars($otherUserPhoto) :
+                                        './assets/images/anonymous.png' ?>"
+                            alt=""
+                            class="w-12 h-12 rounded-full">
+                        <div>
+                            <div>
+                                <h3 class="font-cassian-inter text-[14px]">
+                                    <?= htmlspecialchars($otherUserPseudo ?? '') ?>
+                                </h3>
+                                <p class="font-cassian-inter text-[12px]">
+                                    <?= htmlspecialchars($lastMessageDate ?? '') ?>
+                                </p>
+                            </div>
+                            <p><?= htmlspecialchars($lastMessageContent ?? '') ?></p>
+                        </div>
+
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
         <!-- Discussion messages -->
         <div class="hidden xl:flex w-83.75 xl:flex-1 min-h-full bg-cassian-secondary xl:bg-cassian-primary">
