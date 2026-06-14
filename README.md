45232339c762dd736b61dcbddf2252a2492c7d8c
Jakun9999 <matthieulucas457@outlook.fr>
2026-06-14 11:54:07 +0200

    📦️ Readme and SQL init - Updated readme and sql init file to have demo data available.

--- README.md
+++ README.md
@@ -17,6 +17,26 @@ Before using the project download Docker Desktop and NodeJS.
 - https://www.docker.com/
 - https://nodejs.org/en
 
+## IMPORTANT:
+A set of users, book and messages is available in sql init file.
+Demo user login : demo@demo.com
+Demo user password : Azerty$1
+
+Other existing users use the same password.
+
+Before starting containers make sure to copy both configuration files and 
+define configuration credentials.
+
+```
+cp .env.example .env
+```
+MYSQL_ROOT_PASSWORD and MYSQL_PASSWORD needs to be defined in your new .env.
+
+```
+cp ./www/config/conf.example.php ./www/config/conf.php
+```
+DB_PASS needs to be defined using MYSQL_ROOT_PASSWORD you defined in .env.
+
 ## Technologies
 
 - HTML/CSS/JS
@@ -52,6 +72,17 @@ npx tailwindcss -i ./www/public/assets/css/input.css -o ./www/public/assets/css/
 
 ```
 docker compose up --build -d
+```
+
+## How to stop docker containers without losing data.
+
+```
+docker compose down
+```
+## How to stop docker containers with database removal.
+
+```
+docker compose down -v
 ```
 
 ## Remarks
--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -183,6 +183,34 @@ ALTER TABLE `book`
   FOREIGN KEY (`user_id`) 
   REFERENCES `user` (`id`)
   ON DELETE CASCADE;
+
+--
+-- Demo data upload
+--
+INSERT INTO `user` (`id`, `pseudo`, `email`, `photo`, `password`, `creation_date`) VALUES
+(1, 'matthieu', 'ml@cassiandev.net', '', '$argon2id$v=19$m=65536,t=4,p=1$MGNkSHF0TW5qZjhiVFlRTg$Lev74Tz1srxoXoVZTTwQPASmkctvooYkk/LneQncLyY', '2026-06-12 19:10:29'),
+(2, 'alucas', 'alucas@gmail.com', '', '$argon2id$v=19$m=65536,t=4,p=1$RHVDdnU1QTFxUmg0MDVRZA$oqbM0csyatGs7UNfiBL7m78sCvuj4C1z/vBSHKuoMnA', '2026-06-12 19:10:57'),
+(3, 'demo', 'demo@demo.com', '', '$argon2id$v=19$m=65536,t=4,p=1$dktSUmZIbnNFLnNuUFhyQQ$85QHYb2M9sdjNKUYs8HVr875y4JlwJsNFZNIilzFbjA', '2026-06-14 10:07:45');
+
+INSERT INTO `book` (`id`, `title`, `author`, `author_pseudo`, `description`, `image_url`, `status`, `creation_date`, `user_id`) VALUES
+(1, 'Les règles de l’art', 'M.C Beaton', '', 'Vous aimez Agathe Raisin, vous allez adorer', '', 1, '2026-06-12 19:26:29', 2),
+(2, 'L\'alchimiste', 'Paulo Coelho', '', 'L\'Alchimiste conte le voyage initiatique de Santiago, un jeune berger andalou parti à la recherche d\'un trésor enfoui près des Pyramides d’Égypte. À travers ses rencontres, il apprend à écouter son cœur, à lire les signes du destin et à accomplir sa « Légende Personnelle » pour trouver sa propre vérité.', '', 1, '2026-06-12 19:38:19', 1),
+(3, 'One Punch-Man', 'Yusuke Murata', '', 'Dans le tome 27 de One-Punch Man, l\'affrontement contre l\'Association des Monstres atteint un sommet d\'intensité. Psykos fusionne avec Orochi, le Roi des Monstres, pour donner naissance à une entité d\'une puissance colossale, forçant la redoutable Tatsumaki à repousser ses limites dans un duel dantesque. En parallèle, le terrifiant Garoh continue sa mutation et brise la volonté de Super Black Brillant, terrassé par la peur face à cette force brute. Pendant ce temps, fidèle à lui-même, Saitama reste comiquement à l\'écart de la crise majeure, se retrouvant piégé et enseveli sous un éboulement au fond du labyrinthe souterrain.', '', 0, '2026-06-12 19:40:49', 1),
+(4, 'J\'irai cracher sur vos tombes', 'Boris Vian', '', 'J\'irai cracher sur vos tombes de Boris Vian (écrit sous le pseudonyme de Vernon Sullivan) suit Lee Anderson, un homme noir à la peau blanche. Pour venger le lynchage de son frère, il s\'infiltre dans la jeunesse dorée et raciste d\'une petite ville américaine. Dissimulant son identité, Lee séduit, manipule puis assassine sauvagement deux riches sœurs blanches. Ce roman noir, violent et provocateur, dénonce la ségrégation et le racisme systémique de l\'Amérique des années 1940 à travers une trajectoire de vengeance destructrice qui mènera le protagoniste à sa propre perte.', '', 1, '2026-06-12 19:42:39', 1),
+(5, 'Dracula', 'Bram Stocker', '', 'Dracula n\'est pas le premier roman fantastique à exploiter le thème du vampire. Il marque pourtant une étape cruciale dans la littérature fantastique et en particulier celle abordant le thème des vampires ; le succès du livre et la popularité du personnage l\'attestent encore aujourd\'hui. Plus que le sens du récit et la maîtrise du suspense de Stoker, c\'est la personnalité de son personnage principal qui fonde le mythe. Le comte Dracula, au-delà de la créature d\'épouvante aux pouvoirs surnaturels, est avant tout un être humain damné, un non-mort, et c\'est cette dimension complexe qui assure son charme.', '', 1, '2026-06-14 10:17:23', 3),
+(6, 'Les Vitamines du bonheur', 'Raymond Carver', '', 'Douze nouvelles de Raymond Carver ; Douze univers clos, douze fascinantes variations sur la condition humaine.', '', 0, '2026-06-14 10:20:46', 3);
+
+INSERT INTO `discussion` (`id`, `user_1_id`, `user_2_id`, `creation_date`) VALUES
+(1, 1, 2, '2026-06-12 19:31:16'),
+(2, 3, 1, '2026-06-14 10:09:46');
+
+INSERT INTO `message` (`id`, `user_id`, `creation_date`, `discussion_id`, `content`, `is_read`) VALUES
+(1, 1, '2026-06-12 19:31:16', 1, 'Bonjour, ce livre m\'intéresse beaucoup, on peut prévoir un échange ?', 1),
+(2, 2, '2026-06-12 19:32:03', 1, 'Oui, quel livre me proposez-vous ?', 1),
+(3, 1, '2026-06-12 19:48:08', 1, 'Si vous aimez les policiers, je peux vous conseiller le Boris Vian dont je dispose.', 0),
+(4, 3, '2026-06-14 10:09:46', 2, 'Bonjour, ce livre m\'intéresse.', 1),
+(5, 1, '2026-06-14 10:33:27', 2, 'Top, de mon côté les vitamines du bonheur m\'intéresse, on peut les échanger si vous le souhaitez', 0);
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
